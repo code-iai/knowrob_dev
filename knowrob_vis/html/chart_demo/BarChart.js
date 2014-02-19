dab7478febe24dffc2438a7ab7099d291951b6ff
Arne Stefes <arne.stefes@gmail.com>
2014-02-19 11:39:25 +0100

    some eye candy

--- knowrob_vis/html/chart_demo/BarChart.js
+++ knowrob_vis/html/chart_demo/BarChart.js
@@ -14,22 +14,24 @@ function BarChart (options) {
     svg.append("svg:rect")
         .attr("width", "100%")
         .attr("height", "100%")
-        .attr("stroke", "#000")
+        //.attr("stroke", "#000")
         .attr("fill", "none");
 
     var vis = svg.append("svg:g")
         //.attr("id", "barchart")
         .attr("transform", "translate(50,50)");
 
+    var color = d3.scale.category20();
+
   this.remove = function() {
     svg.remove();
   }
 
-  this.update = function(rawdata) {
+  this.update = function(data) {//rawdata) {
 
-    var data = rawdata.value2;
-    console.log(data);
-    max = d3.max(data, function(d) {return parseInt(d)});
+    //var data = rawdata.value2;
+    console.log(data.value2);
+    max = d3.max(data.value2, function(d) {return parseInt(d)});
     console.log(max);
 
     //nice breakdown of d3 scales
@@ -39,7 +41,7 @@ function BarChart (options) {
         .range([0, w]);
 
     y = d3.scale.ordinal()
-        .domain(d3.range(data.length))
+        .domain(d3.range(data.value2.length))
         .rangeBands([0, h], .2);
 
 
@@ -48,19 +50,20 @@ function BarChart (options) {
     //a good written tutorial of d3 selections coming from protovis
     //http://www.jeromecukier.net/blog/2011/08/09/d3-adding-stuff-and-oh-understanding-selections/
     var bars = vis.selectAll("rect.bar")
-        .data(data);
+        .data(data.value2);
 
     //update
     bars
-        .attr("fill", "#0a0")
-        .attr("stroke", "#050");
+        .attr("fill", function(d, i) { return color(i); });//"#0a0");
+        //.attr("stroke", "black")//#050");
+        //.attr("stroke-width", 0.1);
 
     //enter
     bars.enter()
         .append("svg:rect")
         .attr("class", "bar")
-        .attr("fill", "#0a0") // #800
-        .attr("stroke", "#050"); // #800
+        .attr("fill", function(d, i) { return color(i); });//"#0a0") // #800
+        //.attr("stroke", "#050"); // #800
 
 
     //exit 
