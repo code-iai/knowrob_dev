3c8b698a54b80610d36fca1fc8f1d14cc1b7e399
Andrei Stoica <stoica.razvan.andrei@gmail.com>
2014-08-18 19:18:06 +0200

    Brushed up test of mesh_reasoning: * complemented documentation and code clean-up

--- knowrob_mesh_reasoning/src/edu/tum/cs/test/MeshReasoningTest.java
+++ knowrob_mesh_reasoning/src/edu/tum/cs/test/MeshReasoningTest.java
@@ -3,38 +3,33 @@
  * materials are made available under the terms of the GNU Public License v3.0 which accompanies
  * this distribution, and is available at http://www.gnu.org/licenses/gpl.html
  * 
- * Contributors: Stefan Profanter - initial API and implementation, Year: 2012
+ * Contributors: 
+ * 				Stefan Profanter - initial API and implementation, Year: 2012
+ * 				Andrei Stoica - improved mesh reasoning during Google Summer of Code 2014
  ******************************************************************************/
 package edu.tum.cs.test;
 
 import edu.tum.cs.vis.model.MeshReasoning;
 
 /**
- * Main test class for testing MeshReasoningTest
+ * Main test class for testing MeshReasoningTest with visualization
+ * of the model under test and its reasoning process.
  * 
  * @author Stefan Profanter
- * 
+ * @author Andrei Stoica - refactored main method called analyseByPath(String), {@link edu.tum.cs.vis.model.MeshReasoning#analyseByPath(String)}
  */
 public class MeshReasoningTest {
 
 	/**
 	 * Main Method loading the mesh, drawing it and starting the analyzer
+	 * for only one model. Modify path as needed.
 	 * 
 	 * @param args
 	 *            command line arguments not used
 	 */
 	public static void main(String[] args) {
-		
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models-stefan\\drinking-vessels\\cup2.dae";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models-stefan\\kitchen-tools\\spatula.kmz";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models-stefan\\tools\\paintbrush2.kmz";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models-stefan\\food-drinks\\mondamin-pancake-mix.dae";
-		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models-stefan\\from-3dnet\\hammer\\5b4574dd6fec612e3d2dd82a57bb3bd7.ply";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models-stefan\\from-3dnet\\hammer\\94dbb6874c576ee428bcb63ced82086c.ply";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models\\primitive\\CylinderPart.dae";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models\\primitive\\cone.dae";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models\\primitive\\cylinder.dae";
-//		String path = "C:\\Users\\rxstoi\\work\\ros\\vis\\models\\MeshLabCube.ply";
+		// modify path as desired
+		String path = "/home/andrei/Documents/work/ros/knowrob_cad_models/models/drinking-vessels/cup2.dae";
 		
 		MeshReasoning mr = MeshReasoning.initMeshReasoning(true);
 
